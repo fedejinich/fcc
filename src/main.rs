0b363cb5d6e7672e11b13f49e904b3daecc79a7d
fede <void_rsk@void-rsk.local>
2025-08-06 01:28:22 -0300

    complete preprocessing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{path::Path, process::Command};
 
 use clap::Parser;
 
@@ -19,22 +19,28 @@ struct CompilerDriver {
 
 impl CompilerDriver {
     pub fn create_program(&self) -> Result<(), String> {
-        let _preprocessed_file = self.preprocess(&self.program_path)?;
-        Ok(())
-        // let assembly_file = self.compile(preprocessed_file.as_str())?;
-        // self.assemble_and_link(assembly_file)
+        let preprocessed_file = self.preprocess(&self.program_path)?;
+        let assembly_file = self.compile(preprocessed_file.as_str())?;
+        let exit_code = self.assemble_and_link(assembly_file)?;
+        std::process::exit(exit_code);
     }
 
     pub fn preprocess(&self, source_file: &str) -> Result<String, String> {
+        // todo(fede) this should be validated in another place
         if !source_file.ends_with(".c") {
             return Err(String::from("SOURCE_FILE should have a .c file extension"));
         }
 
+        // todo(fede) this should be tested and is not necesary here
         let preprocessed_file = replace_c_with_i(source_file);
         if !preprocessed_file.ends_with(".i") {
             return Err(String::from(
                 "PREPROCESSED_FILE should have a .i file extension",
             ));
+        }
+
+        if !Path::new(source_file).exists() {
+            return Err(String::from("source file does not exist"));
         }
 
         Command::new("gcc")
@@ -55,16 +61,22 @@ impl CompilerDriver {
         Ok(preprocessed_file.replace(".i", ".asm"))
     }
 
-    fn assemble_and_link(&self, assembly_file: String) -> Result<(), String> {
+    fn assemble_and_link(&self, assembly_file: String) -> Result<i32, String> {
+        if !Path::new(&assembly_file).exists() {
+            return Err(String::from("source file does not exist"));
+        }
         let output_file = assembly_file.replace(".asm", "");
-        Command::new("gcc")
+        let result = Command::new("gcc")
             .arg(assembly_file)
             .arg("-o")
             .arg(output_file)
             .output()
             .expect("failed to assemble and link");
 
-        Ok(())
+        result
+            .status
+            .code()
+            .ok_or(String::from("failed to get status code"))
     }
 }
 
