2bda4f46d950ca333b82909c480c215c2cf5e7d0
fede <void_rsk@void-rsk.local>
2025-08-06 18:01:18 -0300

    testing

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,11 @@ mod util;
 
 fn main() {
     println!("{}\n", Title::title2());
-
-    CompilerDriver::parse()
+    let args = vec!["fcc", "--lex", "return_2.c"];
+    let driver = CompilerDriver::parse_from(args);
+    //let driver = CompilerDriver::parse();
+    println!("{:?}", driver);
+    driver
         .create_program()
         .expect("fcc failed to create program");
 }
