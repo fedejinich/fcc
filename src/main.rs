b38bd63aa83a46424c886589938d850546538312
fede <void_rsk@void-rsk.local>
2025-08-05 23:47:07 -0300

    introduce compiler driver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct CompilerDriver  {
+    program_path: String,
+
+    #[arg(long)]
+    lex: bool,
+
+    #[arg(long)]
+    parse: bool,
+
+    #[arg(long)] // todo this shoulde be code-gen
+    code_gen: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let driver = CompilerDriver::parse();
+    println!("{:?}", driver);
 }
