57a7b5a118286e5162c4ed354286aa4ac0a709d7
fedejinich <jinichfede@gmail.com>
2025-12-22 01:51:56 -0300

    convert tests scripts into cli

--- scripts/compliance_tests.sh
+++ scripts/compliance_tests.sh
@@ -4,24 +4,22 @@ FCC_PATH="$HOME/Projects/fcc/target/debug/fcc"
 
 set -e
 
-echo "Running compliance tests"
+echo "RUNNING COMPLIANCE TESTS"
 
 cd "$(dirname "$0")/.."
-
-echo "Cleaning previous build"
-cargo clean
-
-echo "Building fcc"
-cargo build
 
 cd writing-a-c-compiler-tests/
 
 CHAPTER=6
 
+echo ""
+
 echo "Running latests tests. Chapter $CHAPTER"
 ./test_compiler "$FCC_PATH" -v --chapter $CHAPTER --bitwise --latest-only
 
-# echo "Running all tests"
+echo ""
+
+echo "Running all tests"
 ./test_compiler "$FCC_PATH" -v --chapter $CHAPTER --bitwise
 
-echo "Compliance tests completed successfully!!"
+echo "COMPLIANCE TESTS COMPLETED SUCCESSFULLY!"
