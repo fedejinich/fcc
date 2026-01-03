9ed72c5917c8debd4532f8ce3a6dc212b740e7c7
fedejinich <jinichfede@gmail.com>
2026-01-03 14:23:50 -0300

    refactor(codegen): add helpers for common asm emission patterns

--- src/codegen/x64/from.rs
+++ src/codegen/x64/from.rs
@@ -9,6 +9,52 @@ use crate::{
     },
 };
 
+// helpers for common asm emission patterns
+
+fn emit_conditional_jump(
+    condition: TackyValue,
+    target: TackyIdentifier,
+    jump_when_zero: bool,
+) -> Vec<AsmInstruction> {
+    let cond_code = if jump_when_zero {
+        AsmCondCode::E
+    } else {
+        AsmCondCode::NE
+    };
+    vec![
+        AsmInstruction::Cmp(AsmOperand::Imm(0), AsmOperand::from(condition)),
+        AsmInstruction::JmpCC(cond_code, AsmIdentifier::from(target)),
+    ]
+}
+
+fn emit_relational(
+    op: TackyBinaryOperator,
+    src_1: TackyValue,
+    src_2: TackyValue,
+    dst: TackyValue,
+) -> Vec<AsmInstruction> {
+    vec![
+        AsmInstruction::Cmp(AsmOperand::from(src_2), AsmOperand::from(src_1)),
+        AsmInstruction::Mov(AsmOperand::Imm(0), AsmOperand::from(dst.clone())),
+        AsmInstruction::SetCC(AsmCondCode::from(op), AsmOperand::from(dst)),
+    ]
+}
+
+fn emit_div_rem(
+    is_div: bool,
+    src_1: TackyValue,
+    src_2: TackyValue,
+    dst: TackyValue,
+) -> Vec<AsmInstruction> {
+    let result_reg = if is_div { Reg::AX } else { Reg::DX };
+    vec![
+        AsmInstruction::Mov(AsmOperand::from(src_1), AsmOperand::Register(Reg::AX)),
+        AsmInstruction::Cdq,
+        AsmInstruction::Idiv(AsmOperand::from(src_2)),
+        AsmInstruction::Mov(AsmOperand::Register(result_reg), AsmOperand::from(dst)),
+    ]
+}
+
 impl From<TackyProgram> for AsmProgram {
     fn from(tacky_program: TackyProgram) -> Self {
         AsmProgram {
@@ -47,65 +93,39 @@ impl AsmInstruction {
                 AsmInstruction::Mov(AsmOperand::from(src), AsmOperand::from(dst.clone())),
                 AsmInstruction::Unary(AsmUnaryOperator::from(unary_op), AsmOperand::from(dst)),
             ],
-            TackyInstruction::Binary(op, src_1, src_2, dst) => {
-                let is_div = op == TackyBinaryOperator::Divide;
-                match &op {
-                    // “addition, subtraction, and multiplication,
-                    // we convert a single TACKY instruction into two assembly instructions”
-                    TackyBinaryOperator::Add
-                    | TackyBinaryOperator::Subtract
-                    | TackyBinaryOperator::Multiply
-                    // bitwise operators
-                    | TackyBinaryOperator::BitwiseAnd
-                    | TackyBinaryOperator::BitwiseOr
-                    | TackyBinaryOperator::BitwiseXor
-                    | TackyBinaryOperator::LeftShift
-                    | TackyBinaryOperator::RightShift => vec![
-                        AsmInstruction::Comment(format!("add/sub/mul/bitwise operator: {:?}", op.clone())),
-                        AsmInstruction::Mov(AsmOperand::from(src_1), AsmOperand::from(dst.clone())),
-                        AsmInstruction::Binary(
-                            AsmBinaryOperator::from(op),
-                            AsmOperand::from(src_2),
-                            AsmOperand::from(dst),
-                        ),
-                    ],
-                    // relational operators
-                    TackyBinaryOperator::Equal
-                    | TackyBinaryOperator::NotEqual
-                    | TackyBinaryOperator::GreaterThan
-                    | TackyBinaryOperator::LessThan
-                    | TackyBinaryOperator::LessThanOrEqual
-                    | TackyBinaryOperator::GreaterThanOrEqual => vec![
-                        AsmInstruction::Comment(format!("relational operator: {:?}", op.clone())),
-                        AsmInstruction::Cmp(AsmOperand::from(src_2), AsmOperand::from(src_1)),
-                        AsmInstruction::Mov(AsmOperand::Imm(0), AsmOperand::from(dst.clone())),
-                        AsmInstruction::SetCC(AsmCondCode::from(op), AsmOperand::from(dst)),
-                    ],
-                    TackyBinaryOperator::Divide | TackyBinaryOperator::Remainder => {
-                        let reg = if is_div { AsmOperand::Register(Reg::AX) } else { AsmOperand::Register(Reg::DX) };
-                        vec![
-                            AsmInstruction::Comment(format!("div/rem operator: {:?}", op.clone())),
-                            AsmInstruction::Mov(
-                                AsmOperand::from(src_1),
-                                AsmOperand::Register(Reg::AX),
-                            ),
-                            AsmInstruction::Cdq,
-                            AsmInstruction::Idiv(AsmOperand::from(src_2)),
-                            AsmInstruction::Mov(reg, AsmOperand::from(dst)),
-                        ]
-                    }
-                }
-            }
+            TackyInstruction::Binary(op, src_1, src_2, dst) => match op {
+                // arithmetic and bitwise
+                TackyBinaryOperator::Add
+                | TackyBinaryOperator::Subtract
+                | TackyBinaryOperator::Multiply
+                | TackyBinaryOperator::BitwiseAnd
+                | TackyBinaryOperator::BitwiseOr
+                | TackyBinaryOperator::BitwiseXor
+                | TackyBinaryOperator::LeftShift
+                | TackyBinaryOperator::RightShift => vec![
+                    AsmInstruction::Mov(AsmOperand::from(src_1), AsmOperand::from(dst.clone())),
+                    AsmInstruction::Binary(
+                        AsmBinaryOperator::from(op),
+                        AsmOperand::from(src_2),
+                        AsmOperand::from(dst),
+                    ),
+                ],
+                // relational
+                TackyBinaryOperator::Equal
+                | TackyBinaryOperator::NotEqual
+                | TackyBinaryOperator::GreaterThan
+                | TackyBinaryOperator::LessThan
+                | TackyBinaryOperator::LessThanOrEqual
+                | TackyBinaryOperator::GreaterThanOrEqual => emit_relational(op, src_1, src_2, dst),
+                // division and remainder
+                TackyBinaryOperator::Divide => emit_div_rem(true, src_1, src_2, dst),
+                TackyBinaryOperator::Remainder => emit_div_rem(false, src_1, src_2, dst),
+            },
             TackyInstruction::Jump(id) => vec![AsmInstruction::Jmp(AsmIdentifier::from(id))],
-            // TODO: this is almost same as JumpIfNotZero
-            TackyInstruction::JumpIfZero(condition, target) => vec![
-                AsmInstruction::Cmp(AsmOperand::Imm(0), AsmOperand::from(condition)),
-                AsmInstruction::JmpCC(AsmCondCode::E, AsmIdentifier::from(target)),
-            ],
-            TackyInstruction::JumpIfNotZero(condition, target) => vec![
-                AsmInstruction::Cmp(AsmOperand::Imm(0), AsmOperand::from(condition)),
-                AsmInstruction::JmpCC(AsmCondCode::NE, AsmIdentifier::from(target)),
-            ],
+            TackyInstruction::JumpIfZero(cond, target) => emit_conditional_jump(cond, target, true),
+            TackyInstruction::JumpIfNotZero(cond, target) => {
+                emit_conditional_jump(cond, target, false)
+            }
             TackyInstruction::Copy(src, dst) => vec![AsmInstruction::Mov(
                 AsmOperand::from(src),
                 AsmOperand::from(dst),
@@ -137,7 +157,6 @@ impl From<TackyUnaryOperator> for AsmUnaryOperator {
         match tacky_unary_operator {
             TackyUnaryOperator::Negate => AsmUnaryOperator::Neg,
             TackyUnaryOperator::Complement => AsmUnaryOperator::Not,
-            // logical unary operators
             TackyUnaryOperator::Not => AsmUnaryOperator::Not,
         }
     }
